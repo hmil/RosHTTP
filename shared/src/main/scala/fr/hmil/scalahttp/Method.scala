9576183b2cc8d92b4c6091315106fd5698235987
Hadrien Milano <hadrien.milano@gmail.com>
2016-06-01 16:52:39 +0200

    refactor Method class (#21)

--- shared/src/main/scala/fr/hmil/scalahttp/Method.scala
+++ shared/src/main/scala/fr/hmil/scalahttp/Method.scala
@@ -1,7 +1,7 @@
 package fr.hmil.scalahttp
 
 /** Wraps HTTP method strings. */
-final case class Method private (private val name: String) {
+final class Method private(private val name: String) {
 
   override def toString: String = name.toUpperCase
 
@@ -21,36 +21,14 @@ final case class Method private (private val name: String) {
   * with other HTTP methods, please submit an issue with your motivation.
   */
 object Method {
-
   val GET = Method("GET")
   val POST = Method("POST")
   val HEAD = Method("HEAD")
   val OPTIONS = Method("OPTIONS")
   val PUT = Method("PUT")
   val DELETE = Method("DELETE")
+  val PATCH = Method("PATCH")
   val TRACE = Method("TRACE")
 
-  object Implicits {
-    /** Transform a method string into a [[Method]] instance.
-      *
-      * Given that this library is very unlikely to be
-      * used with other more exotic methods, if the user specifies an unsupported method,
-      * it is probably a typo rather than an actual HTTP method. We therefore report the
-      * error early as an IllegalArgumentException
-      *
-      * @param name "GET", "POST", "HEAD", "OPTIONS", "PUT", "DELETE" or "TRACE"; Case insensitive.
-      * @throws IllegalArgumentException when name is none of the legal values.
-      * @return The method instance for the provided method name.
-      */
-    implicit def fromString(name: String): Method = name.toUpperCase match {
-      case "GET" => GET
-      case "POST" => POST
-      case "HEAD" => HEAD
-      case "OPTIONS" => OPTIONS
-      case "PUT" => PUT
-      case "DELETE" => DELETE
-      case "TRACE" => TRACE
-      case _ => throw new IllegalArgumentException(s"Invalid HTTP method: $name")
-    }
-  }
+  def apply(name: String): Method = new Method(name)
 }
