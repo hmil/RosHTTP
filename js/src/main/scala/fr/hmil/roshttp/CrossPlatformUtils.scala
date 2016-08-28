ed109e2d4ae4bfa69be4210ab614f932a7bf4c31
Hadrien Milano <hadrien.milano@gmail.com>
2016-08-28 16:22:31 -0700

    Fix #43, don't escape '&' and '=' characters in query string

--- js/src/main/scala/fr/hmil/roshttp/CrossPlatformUtils.scala
+++ js/src/main/scala/fr/hmil/roshttp/CrossPlatformUtils.scala
@@ -5,9 +5,9 @@ import scala.scalajs.js
 
 private object CrossPlatformUtils {
 
-  def encodeQueryString(query: String): String =
+  def encodeURIComponent(query: String): String =
     js.URIUtils.encodeURIComponent(query)
 
-  def decodeQueryString(query: String): String =
+  def decodeURIComponent(query: String): String =
     js.URIUtils.decodeURIComponent(query)
 }
