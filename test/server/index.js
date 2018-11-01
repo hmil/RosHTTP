e41fbd94c16af62c43d97617dea3e0c7a0708c7f
Andreas Bauer <baueran@gmail.com>
2018-11-01 08:04:39 +0100

    Add preflight to cors-module and README tests to test spec

--- test/server/index.js
+++ test/server/index.js
@@ -11,7 +11,12 @@ var cors = require('cors')
 
 app.use(morgan('combined'));
 app.use(cookieParser())
-app.use(cors({credentials: true, origin: true}))
+app.use(cors({
+    credentials: true,
+    preflightContinue: true,
+    origin: true
+  })
+)
 app.use(multipart());
 
 // parse application/x-www-form-urlencoded
@@ -165,7 +170,7 @@ app.post('/streams/in', function(req, res) {
 app.get('/set_cookie', function(req, res) {
   if (req.query.token !== undefined || req.cookies === undefined || Object.keys(req.cookies).length === 0) {
     res.cookie('test_cookie', req.query.token);
-	res.status(200).send("Cookie request answered by sending cookie client.");
+	res.status(200).send("Cookie request answered by sending cookie to client.");
   } else {
     res.status(500).send('Server sent no cookie to client!')
   }
@@ -179,7 +184,7 @@ app.get('/verify_cookie', function(req, res) {
     if (testCookie === req.query.token) {
       res.status(200).send('Cookie received from client.')
     } else {
-      res.status(500).send('A cookie received from client, but contained data was wrong!')
+      res.status(500).send('Cookie received from client, but contained data was wrong!')
     }
   }
 });
